88ad04b6dd1dfb45f7ab786691ade10d01b7ecb8
ahmadgotnochill <dimtimstars99@gmail.com>
2026-06-01 00:28:39 +0500

    Fix trailing slash style paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/tamseelFatima',
+  assetPrefix: '/tamseelFatima/', // <-- Added a trailing slash to fix the CSS paths!
   images: {
     unoptimized: true,
   },
